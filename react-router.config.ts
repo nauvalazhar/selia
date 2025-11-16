a5ada33b2d39f1aa8516a636cdcb976edfb06862
Nauval <nauvalazhar2@gmail.com>
2025-11-16 20:14:49 +0700

    fix prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,6 +5,17 @@ import path from 'node:path';
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  prerender: true,
+  async prerender({ getStaticPaths }) {
+    const components = await readdir(
+      path.join(import.meta.dirname, 'components/selia'),
+    );
+
+    return [
+      ...getStaticPaths(),
+      ...components.map(
+        (component) => `/docs/${component.replace('.tsx', '')}`,
+      ),
+    ];
+  },
   ssr: false,
 } satisfies Config;
