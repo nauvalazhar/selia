de681d72145e36a431f226ee35bd35fdea4ae99a
Nauval <nauvalazhar2@gmail.com>
2025-11-24 10:10:24 +0700

    fix variant

--- app/routes/playground.tsx
+++ app/routes/playground.tsx
@@ -37,115 +37,13 @@ import { Badge } from 'components/selia/badge';
 
 export default function SidebarBasicExample() {
   return (
-    <div className="flex gap-4 h-screen">
-      <Sidebar className="lg:w-72" size="loose">
-        <SidebarHeader>
-          <SidebarLogo>
-            <img src="/selia.png" alt="Selia" className="size-8" />
-            <span className="font-semibold text-foreground">Selia</span>
-          </SidebarLogo>
-        </SidebarHeader>
-        <SidebarContent>
-          <SidebarMenu>
-            <SidebarGroup>
-              <SidebarGroupTitle>Navigation</SidebarGroupTitle>
-              <SidebarGroupAction>
-                <button className="text-foreground">
-                  <PlusIcon />
-                </button>
-              </SidebarGroupAction>
-              <SidebarList line>
-                <SidebarItem href="#" active>
-                  <HomeIcon />
-                  Dashboard
-                </SidebarItem>
-                <SidebarItem href="#">
-                  <ShoppingBagIcon />
-                  Products
-                </SidebarItem>
-                <SidebarItem>
-                  <TagsIcon />
-                  Categories
-                </SidebarItem>
-                <SidebarItem>
-                  <Package2Icon />
-                  Orders
-                  <Badge className="ml-auto" size="sm" pill variant="info">
-                    10
-                  </Badge>
-                </SidebarItem>
-                <SidebarCollapsible>
-                  <SidebarCollapsibleTrigger>
-                    <ChartAreaIcon />
-                    Reports
-                  </SidebarCollapsibleTrigger>
-                  <SidebarSubmenu>
-                    <SidebarList>
-                      <SidebarItem href="#">Sales</SidebarItem>
-                      <SidebarItem href="#">Traffic</SidebarItem>
-                      <SidebarItem href="#">Conversion</SidebarItem>
-                    </SidebarList>
-                  </SidebarSubmenu>
-                </SidebarCollapsible>
-              </SidebarList>
-            </SidebarGroup>
-            <SidebarGroup>
-              <SidebarGroupTitle>Settings</SidebarGroupTitle>
-              <SidebarList>
-                <SidebarItem href="#">
-                  <SettingsIcon />
-                  Settings
-                </SidebarItem>
-                <SidebarItem href="#">
-                  <UserIcon />
-                  Profile
-                </SidebarItem>
-              </SidebarList>
-            </SidebarGroup>
-          </SidebarMenu>
-        </SidebarContent>
-        <SidebarFooter>
-          <SidebarMenu>
-            <SidebarList>
-              <Dropdown>
-                <DropdownTrigger
-                  data-slot="sidebar-item"
-                  nativeButton={false}
-                  render={
-                    <SidebarItem>
-                      <Avatar size="sm">
-                        <AvatarImage src="/avatar05.png" alt="Avatar" />
-                        <AvatarFallback>BS</AvatarFallback>
-                      </Avatar>
-                      <div className="flex flex-col">
-                        <span className="font-medium">John Doe</span>
-                        <span className="text-sm text-muted">
-                          john.doe@example.com
-                        </span>
-                      </div>
-                      <ChevronsUpDownIcon className="ml-auto" />
-                    </SidebarItem>
-                  }
-                />
-                <DropdownContent className="w-(--anchor-width)" side="top">
-                  <DropdownItem>
-                    <UserIcon />
-                    Profile
-                  </DropdownItem>
-                  <DropdownItem>
-                    <SettingsIcon />
-                    Settings
-                  </DropdownItem>
-                  <DropdownItem>
-                    <LogOutIcon />
-                    Logout
-                  </DropdownItem>
-                </DropdownContent>
-              </Dropdown>
-            </SidebarList>
-          </SidebarMenu>
-        </SidebarFooter>
-      </Sidebar>
+    <div className="flex gap-4 h-screen w-screen">
+      <div className="dark p-10 bg-background size-60 text-foreground">
+        Dark
+        <div className="light p-10 bg-background size-60 text-foreground">
+          Not Dark
+        </div>
+      </div>
     </div>
   );
 }
