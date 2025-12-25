6b1aee410e6d2b75d6e9cdab9c345a351b2bebbf
Nauval <nauvalazhar2@gmail.com>
2025-12-25 08:40:47 +0700

    fix: collapsible example

--- components/examples/collapsible/basic.tsx
+++ components/examples/collapsible/basic.tsx
@@ -8,7 +8,7 @@ import { Text } from 'components/selia/text';
 export default function CollapsibleBasicExample() {
   return (
     <div className="flex items-center justify-center h-40 w-full">
-      <Collapsible className="w-full lg:w-8/12">
+      <Collapsible className="w-full lg:w-7/12 xl:w-5/12">
         <CollapsibleTrigger>What is Linux?</CollapsibleTrigger>
         <CollapsiblePanel>
           <Text className="text-muted">
