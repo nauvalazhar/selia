d4087b53eb0451213949c781c11bacfb1fc3f009
Nauval <nauvalazhar2@gmail.com>
2026-01-29 15:09:45 +0700

    fix: refactor utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { blocks } from 'components/blocks';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function categoryToSlug(category: string): string {
-  return category.toLowerCase().replace(/\s+/g, '-');
-}
-
-export function slugToCategory(slug: string): string {
-  const categories = [...new Set(Object.values(blocks).map(b => b.category))];
-  return categories.find(c => categoryToSlug(c) === slug) || '';
 }
